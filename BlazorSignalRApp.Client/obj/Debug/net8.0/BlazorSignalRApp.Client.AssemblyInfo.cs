f5fd1a606a2e0a3d8e90067d5d8eacacb8cb1729
unknown <aristotepascaldjounda@gmail.com>
2025-04-18 10:32:09 +0200

    finish implementing do not disturb

--- BlazorSignalRApp.Client/obj/Debug/net8.0/BlazorSignalRApp.Client.AssemblyInfo.cs
+++ BlazorSignalRApp.Client/obj/Debug/net8.0/BlazorSignalRApp.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorSignalRApp.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dd1622af7e007b3175691c289cc053323829b88")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorSignalRApp.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorSignalRApp.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- BlazorSignalRApp/Controllers/SocketController.cs
+++ BlazorSignalRApp/Controllers/SocketController.cs
@@ -1,16 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
 using BlazorSignalRApp.Hubs;
+using System.Collections.Concurrent;
+
 
 [ApiController]
 [Route("api/[controller]")]
 public class SocketController : ControllerBase
 {
     private readonly IHubContext<ChatHub> _hubContext;
+    private static ConcurrentDictionary<string, bool> userDndStates = new();
+    private static bool doNotDisturb=false;
 
     public SocketController(IHubContext<ChatHub> hubContext)
     {
         _hubContext = hubContext;
+        //doNotDisturb = false;
+        Console.Write("Controller launch");
     }
 
     [HttpGet]
@@ -22,9 +28,24 @@ public class SocketController : ControllerBase
     [HttpPost]
     public async Task<IActionResult> SendNotification([FromBody] NotificationDto dto)
     {
+
+        // Vérifie si l'utilisateur est en DND
+        if (doNotDisturb)
+        {
+            return Ok("dnd mode");
+        }
         // Send the message to all connected SignalR clients
         await _hubContext.Clients.All.SendAsync("ReceiveMessage", dto.User, dto.Message);
-        return Ok("Notification sent");
+        return Ok("sent");
+    }
+
+    [HttpGet("dnd")]
+    public IActionResult SetDoNotDisturb()
+    {
+        Console.WriteLine(doNotDisturb);
+        doNotDisturb = !doNotDisturb;
+        Console.WriteLine(doNotDisturb);
+        return Ok($"dnd mode {(doNotDisturb ? "switched on" : "switched off") }");
     }
 }
 
@@ -33,4 +54,14 @@ public class NotificationDto
     public string User { get; set; }
     public string Message { get; set; }
 
+}
+public class DndDto
+{
+    public string User { get; set; }
+    public bool IsDnd { get; set; }
+}
+
+public class Dnd
+{
+    public bool isDnd;
 }
